3cc705398a6ece0fdd3e235f9d207a4a4d6548bf
XFireYTh <thomasjefferson134@gmail.com>
2026-04-10 10:30:58 -0300

    Construção inicial do Calendario e da Agenda + Estilizações e Animações

---
+++ SUS-Plus/js/script.js
@@ -0,0 +1,47 @@
+// Constantes Necessárias
+
+const dias = document.getElementById("dias")
+
+// --------------------------------------------
+
+// Variáveis Necessárias
+
+
+
+// --------------------------------------------
+
+// Funções Necessárias
+
+// --------------------------------------------
+
+// Funções Específicas
+
+function meses(mes) {
+    const meses = [
+        "Janeiro", "Fevereiro", "Março", "Abril",
+        "Maio", "Junho", "Julho", "Agosto",
+        "Setembro", "Outubro", "Novembro", "Dezembro"
+    ]
+    return meses[mes]
+}
+
+function formarCalendario(tabela_DOM) {
+    let data = new Date()
+    const diaAtual = data.getDate()
+    const mesAtual = data.getMonth()
+    const anoAtual = data.getFullYear()
+    console.log(`${diaAtual}/${mesAtual}/${anoAtual}`)
+    console.log(meses(mesAtual))
+}
+
+// --------------------------------------------
+
+// Área de Eventos
+
+// --------------------------------------------
+
+// Área de Automação
+
+formarCalendario(dias)
+
+// --------------------------------------------
