b8d8b19e8a344f290b3799f77c8f9533a1736a6e
XFireYTh <thomasjefferson134@gmail.com>
2026-04-10 22:22:55 -0300

    Sincronização do Calendário e correções gerais

--- SUS-Plus/js/script.js
+++ SUS-Plus/js/script.js
@@ -1,6 +1,6 @@
 // Constantes Necessárias
 
-const dias = document.getElementById("dias")
+const dias = document.getElementById("dias_resumo")
 
 // --------------------------------------------
 
@@ -35,6 +35,56 @@ function formarCalendario(tabela_DOM) {
     console.log(meses(mesAtual))
 
     document.getElementById("mes_resumo").innerHTML = `${meses(mesAtual)} ${anoAtual}`
+
+    let semanas = 0
+    let semanaDiaCounter = 0
+    let dia = 1
+    let limiteDias = new Date(anoAtual, mesAtual + 1, 0).getDate()
+    let diaSemanaTrava = new Date(anoAtual, mesAtual, 1).getDay()
+
+    while (semanas < 6 || dia < limiteDias) {
+        if (diaSemanaTrava > 6) {
+            diaSemanaTrava = 0
+        }
+        if (semanaDiaCounter > 6) {
+            semanaDiaCounter = 0
+        }
+        dias.innerHTML += `
+            <tr id="${semanas}sc">
+
+            </tr>
+        `
+
+        while (semanaDiaCounter <= 6) {
+            if (semanaDiaCounter == diaSemanaTrava && dia <= limiteDias) {
+                document.getElementById(`${semanas}sc`).innerHTML += celulaPreenchida(dia)
+                dia++
+                semanaDiaCounter++
+                diaSemanaTrava++
+            } else {
+                document.getElementById(`${semanas}sc`).innerHTML += celulaVazia()
+                semanaDiaCounter++
+            }
+        }
+
+        semanas++
+        
+    }
+
+    let monitorDias = 0
+    for(let td in document.getElementById(`${semanas}sc`).innerHTML) {
+        if (monitorDias == 0) {
+            td.id = "dia_inicial_semana"
+        }
+    }
+}
+
+function celulaVazia() {
+    return "<td></td>"
+}
+
+function celulaPreenchida(valor) {
+    return `<td>${valor}</td>`
 }
 
 // --------------------------------------------
